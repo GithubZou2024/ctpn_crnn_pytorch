a12cd11786980e44e4c7fcc09d7906e4f3bd2f63
GithubZou2024 <162770658+GithubZou2024@users.noreply.github.com>
2026-04-03 12:17:12 +0800

    modefied pretrained weights path

--- train_code/train_ctpn/config.py
+++ train_code/train_ctpn/config.py
@@ -44,7 +44,10 @@ else:
     print("未检测到GPU, 使用CPU进行训练")
 
 # ========== 【模型训练参数】==========
-pretrained_weights = kaggle_root+'/kaggle/input/datasets/pretriained-ctpn'
+if IS_KAGGLE:
+    pretrained_weights = '/kaggle/input/datasets/zouhahaha/pretrained-ctpn/CTPN.pth'
+else:
+    pretrained_weights = kaggle_root + '/kaggle/input/datasets/pretriained-ctpn/CTPN.pth'
 anchor_scale = 16
 IOU_NEGATIVE = 0.3
 IOU_POSITIVE = 0.7
